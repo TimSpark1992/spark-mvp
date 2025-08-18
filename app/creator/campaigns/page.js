534d222d2b4ed566f4b6765804d46e7a5abc9b80
Emergent Agent <emergent@example.com>
2025-08-18 15:18:34 +0000

    auto-commit for b85c62ff-5d29-4b73-ab44-0dc07e3273a8

--- app/creator/campaigns/page.js
+++ app/creator/campaigns/page.js
@@ -202,7 +202,7 @@ export default function CreatorCampaignsPage() {
                   <SelectValue placeholder="All Categories" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="">All Categories</SelectItem>
+                  <SelectItem value="all">All Categories</SelectItem>
                   {categories.map((category) => (
                     <SelectItem key={category} value={category}>
                       {category}
